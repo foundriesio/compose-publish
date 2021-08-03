bfa0027e4b0eecfa3b0942874d6a43a99a9a87c8
Andy Doan <andy@foundries.io>
2021-08-03 16:53:56 -0500

    Move helper to public package
    This allows it to be shared with other tooling
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"strings"
 
-	"github.com/compose-spec/compose-go/loader"
-	compose "github.com/compose-spec/compose-go/types"
-	"github.com/docker/docker/client"
 	commandLine "github.com/urfave/cli/v2"
 
-	"github.com/foundriesio/compose-publish/internal"
+	"github.com/foundriesio/compose-publish/pkg"
 )
 
 const banner = `
@@ -59,7 +53,7 @@ func main() {
 			if len(target) == 0 {
 				return errors.New("Missing required argument: TARGET:[TAG]")
 			}
-			return doPublish(file, target, digestFile, dryRun)
+			return pkg.DoPublish(file, target, digestFile, dryRun)
 		},
 	}
 
@@ -67,75 +61,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func getClient() (*client.Client, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		return nil, err
-	}
-	cli.NegotiateAPIVersion(context.Background())
-	return cli, nil
-}
-
-func loadProj(file string, config map[string]interface{}) (*compose.Project, error) {
-	env := make(map[string]string)
-	for _, val := range os.Environ() {
-		parts := strings.Split(val, "=")
-		env[parts[0]] = parts[1]
-	}
-
-	var files []compose.ConfigFile
-	files = append(files, compose.ConfigFile{Filename: file, Config: config})
-	return loader.Load(compose.ConfigDetails{
-		WorkingDir:  ".",
-		ConfigFiles: files,
-		Environment: env,
-	})
-}
-
-func doPublish(file, target, digestFile string, dryRun bool) error {
-	b, err := ioutil.ReadFile(file)
-	if err != nil {
-		return err
-	}
-	config, err := loader.ParseYAML(b)
-	if err != nil {
-		return err
-	}
-	cli, err := getClient()
-	if err != nil {
-		return err
-	}
-
-	ctx := context.Background()
-
-	proj, err := loadProj(file, config)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("= Pinning service images...")
-	svcs, ok := config["services"]
-	if !ok {
-		return errors.New("Unable to find 'services' section of compose file")
-	}
-	if err := internal.PinServiceImages(cli, ctx, svcs.(map[string]interface{}), proj); err != nil {
-		return err
-	}
-
-	fmt.Println("== Hashing services...")
-	if err := internal.PinServiceConfigs(cli, ctx, svcs.(map[string]interface{}), proj); err != nil {
-		return err
-	}
-
-	fmt.Println("= Publishing app...")
-	dgst, err := internal.CreateApp(ctx, config, target, dryRun)
-	if err != nil {
-		return err
-	}
-	if len(digestFile) > 0 {
-		return ioutil.WriteFile(digestFile, []byte(dgst), 0o640)
-	}
-	return nil
 }
