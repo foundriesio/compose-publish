438f522d2eb5cb1eabc25853f98ca82f0b55ab44
Eric Bode <eric.bode@foundries.io>
2024-04-15 20:19:14 +0200

    fix(internal): Use new internal code
    Signed-off-by: Eric Bode <eric.bode@foundries.io>

--- internal/reg_client.go
+++ internal/reg_client.go
@@ -5,23 +5,26 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
+	distribution "github.com/distribution/distribution/v3"
+
+	"github.com/distribution/reference"
 	"github.com/docker/cli/cli/config"
-	"github.com/docker/distribution"
-	"github.com/docker/distribution/reference"
-	distributionclient "github.com/docker/distribution/registry/client"
-	"github.com/docker/distribution/registry/client/auth"
-	"github.com/docker/distribution/registry/client/transport"
-	"github.com/docker/docker/api/types"
 	registrytypes "github.com/docker/docker/api/types/registry"
-	"github.com/docker/docker/registry"
+	"github.com/foundriesio/compose-publish/internal/client"
+	"github.com/foundriesio/compose-publish/internal/client/auth"
+	"github.com/foundriesio/compose-publish/internal/client/auth/challenge"
+	"github.com/foundriesio/compose-publish/internal/client/transport"
+	mobyregistry "github.com/moby/moby/registry"
 	"github.com/pkg/errors"
 )
 
 // AuthConfigResolver returns Auth Configuration for an index
-type AuthConfigResolver func(ctx context.Context, index *registrytypes.IndexInfo) types.AuthConfig
+type AuthConfigResolver func(ctx context.Context, index *registrytypes.IndexInfo) registrytypes.AuthConfig
 
 type RegistryClient struct {
 	authConfigResolver AuthConfigResolver
@@ -29,14 +32,14 @@ type RegistryClient struct {
 	userAgent          string
 }
 
-func ResolveAuthConfig(ctx context.Context, index *registrytypes.IndexInfo) types.AuthConfig {
+func ResolveAuthConfig(ctx context.Context, index *registrytypes.IndexInfo) registrytypes.AuthConfig {
 	cfg := config.LoadDefaultConfigFile(os.Stderr)
 	a, _ := cfg.GetAuthConfig(index.Name)
-	return types.AuthConfig(a)
+	return registrytypes.AuthConfig(a)
 }
 
 func NewRegistryClient() RegistryClient {
-	resolver := func(ctx context.Context, index *registrytypes.IndexInfo) types.AuthConfig {
+	resolver := func(ctx context.Context, index *registrytypes.IndexInfo) registrytypes.AuthConfig {
 		return ResolveAuthConfig(ctx, index)
 	}
 
@@ -65,7 +68,7 @@ func (c *RegistryClient) getRepositoryForReference(ctx context.Context, ref refe
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to parse repo name from %s", ref)
 	}
-	return distributionclient.NewRepository(repoName, repoEndpoint.BaseURL(), httpTransport)
+	return client.NewRepository(repoName, repoEndpoint.BaseURL(), httpTransport)
 }
 
 func (c *RegistryClient) getHTTPTransportForRepoEndpoint(ctx context.Context, repoEndpoint repositoryEndpoint) (http.RoundTripper, error) {
@@ -77,8 +80,35 @@ func (c *RegistryClient) getHTTPTransportForRepoEndpoint(ctx context.Context, re
 	return httpTransport, errors.Wrap(err, "failed to configure transport")
 }
 
+// PingV2Registry attempts to ping a v2 registry and on success return a
+// challenge manager for the supported authentication types.
+// If a response is received but cannot be interpreted, a PingResponseError will be returned.
+func PingV2Registry(endpoint *url.URL, transport http.RoundTripper) (challenge.Manager, error) {
+	pingClient := &http.Client{
+		Transport: transport,
+		Timeout:   15 * time.Second,
+	}
+	endpointStr := strings.TrimRight(endpoint.String(), "/") + "/v2/"
+	req, err := http.NewRequest(http.MethodGet, endpointStr, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pingClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	challengeManager := challenge.NewSimpleManager()
+	if err := challengeManager.AddResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return challengeManager, nil
+}
+
 // getHTTPTransport builds a transport for use in communicating with a registry
-func getHTTPTransport(authConfig types.AuthConfig, endpoint registry.APIEndpoint, repoName string, userAgent string) (http.RoundTripper, error) {
+func getHTTPTransport(authConfig registrytypes.AuthConfig, endpoint mobyregistry.APIEndpoint, repoName string, userAgent string) (http.RoundTripper, error) {
 	// get the http transport, this will be used in a client to upload manifest
 	base := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
@@ -92,20 +122,22 @@ func getHTTPTransport(authConfig types.AuthConfig, endpoint registry.APIEndpoint
 		DisableKeepAlives:   true,
 	}
 
-	modifiers := registry.Headers(userAgent, http.Header{})
+	modifiers := []transport.RequestModifier{}
+	if userAgent != "" {
+		modifiers = append(modifiers, transport.NewHeaderRequestModifier(http.Header{
+			"User-Agent": []string{userAgent},
+		}))
+	}
 	authTransport := transport.NewTransport(base, modifiers...)
-	challengeManager, confirmedV2, err := registry.PingV2Registry(endpoint.URL, authTransport)
+	challengeManager, err := PingV2Registry(endpoint.URL, authTransport)
 	if err != nil {
 		return nil, errors.Wrap(err, "error pinging v2 registry")
-	}
-	if !confirmedV2 {
-		return nil, fmt.Errorf("unsupported registry version")
 	}
 	if authConfig.RegistryToken != "" {
 		passThruTokenHandler := &existingTokenHandler{token: authConfig.RegistryToken}
 		modifiers = append(modifiers, auth.NewAuthorizer(challengeManager, passThruTokenHandler))
 	} else {
-		creds := registry.NewStaticCredentialStore(&authConfig)
+		creds := mobyregistry.NewStaticCredentialStore(&authConfig)
 		tokenHandler := auth.NewTokenHandler(authTransport, creds, repoName, "push", "pull")
 		basicHandler := auth.NewBasicHandler(creds)
 		modifiers = append(modifiers, auth.NewAuthorizer(challengeManager, tokenHandler, basicHandler))
@@ -127,8 +159,8 @@ func (th *existingTokenHandler) Scheme() string {
 }
 
 type repositoryEndpoint struct {
-	info     *registry.RepositoryInfo
-	endpoint registry.APIEndpoint
+	info     *mobyregistry.RepositoryInfo
+	endpoint mobyregistry.APIEndpoint
 }
 
 // Name returns the repository name
@@ -147,7 +179,7 @@ func (r repositoryEndpoint) BaseURL() string {
 }
 
 func newDefaultRepositoryEndpoint(ref reference.Named, insecure bool) (repositoryEndpoint, error) {
-	repoInfo, err := registry.ParseRepositoryInfo(ref)
+	repoInfo, err := mobyregistry.ParseRepositoryInfo(ref)
 	if err != nil {
 		return repositoryEndpoint{}, err
 	}
@@ -161,17 +193,17 @@ func newDefaultRepositoryEndpoint(ref reference.Named, insecure bool) (repositor
 	return repositoryEndpoint{info: repoInfo, endpoint: endpoint}, nil
 }
 
-func getDefaultEndpointFromRepoInfo(repoInfo *registry.RepositoryInfo) (registry.APIEndpoint, error) {
+func getDefaultEndpointFromRepoInfo(repoInfo *mobyregistry.RepositoryInfo) (mobyregistry.APIEndpoint, error) {
 	var err error
 
-	options := registry.ServiceOptions{}
-	registryService, err := registry.NewService(options)
+	options := mobyregistry.ServiceOptions{}
+	registryService, err := mobyregistry.NewService(options)
 	if err != nil {
-		return registry.APIEndpoint{}, err
+		return mobyregistry.APIEndpoint{}, err
 	}
 	endpoints, err := registryService.LookupPushEndpoints(reference.Domain(repoInfo.Name))
 	if err != nil {
-		return registry.APIEndpoint{}, err
+		return mobyregistry.APIEndpoint{}, err
 	}
 	// Default to the highest priority endpoint to return
 	endpoint := endpoints[0]
